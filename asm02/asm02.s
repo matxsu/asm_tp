088153f1222022a613791750831c9b6e411f7d7b
Mathis NGO <m.ngo@myskolae.fr>
2026-04-03 12:27:03 +0200

    first commit: 0-5

---
+++ asm02/asm02.s
@@ -0,0 +1,41 @@
+section .data
+    ok_msg  db "1337", 10
+
+section .bss
+    buf     resb 16
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buf
+    mov rdx, 16
+    syscall
+
+    cmp rax, 3
+    jne .fail
+
+    cmp byte [buf],     '4'
+    jne .fail
+    cmp byte [buf + 1], '2'
+    jne .fail
+    mov al, [buf + 2]
+    cmp al, 10
+    jne .fail
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, ok_msg
+    mov rdx, 5
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+.fail:
+    mov rax, 60
+    mov rdi, 1
+    syscall
