adc473ada7affb7570bc9143d2a5133d06150890
Mathis NGO <m.ngo@myskolae.fr>
2026-04-10 13:50:45 +0200

    feat: asm10

---
+++ asm10/asm10.s
@@ -0,0 +1,86 @@
+section .bss
+    outbuf resb 32
+
+section .text
+    global _start
+
+atoi:
+    xor rax, rax
+.loop:
+    movzx rbx, byte [rsi]
+    cmp rbx, 0
+    je .done
+    sub rbx, '0'
+    imul rax, 10
+    add rax, rbx
+    inc rsi
+    jmp .loop
+.done:
+    ret
+
+itoa:
+    lea rdi, [outbuf + 30]
+    mov byte [rdi], 10
+    dec rdi
+    xor rcx, rcx
+    test rax, rax
+    jnz .cv
+    mov byte [rdi], '0'
+    dec rdi
+    inc rcx
+    jmp .dn
+.cv:
+    xor rdx, rdx
+    mov rbx, 10
+    div rbx
+    add dl, '0'
+    mov [rdi], dl
+    dec rdi
+    inc rcx
+    test rax, rax
+    jnz .cv
+.dn:
+    inc rdi
+    mov rsi, rdi
+    mov rdx, rcx
+    inc rdx
+    ret
+
+_start:
+    cmp qword [rsp], 4
+    jne .fail
+
+    mov rsi, [rsp + 16]
+    call atoi
+    mov r12, rax
+
+    mov rsi, [rsp + 24]
+    call atoi
+    mov r13, rax
+
+    mov rsi, [rsp + 32]
+    call atoi
+    mov r14, rax
+
+    mov rax, r12
+    cmp r13, rax
+    jle .ab_done
+    mov rax, r13
+.ab_done:
+    cmp r14, rax
+    jle .done
+    mov rax, r14
+.done:
+    call itoa
+    mov rax, 1
+    mov rdi, 1
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+.fail:
+    mov rax, 60
+    mov rdi, 1
+    syscall
